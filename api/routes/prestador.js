fb289b1eac9ddc50ead28b11124c731e6bdfd2cb
ERICK1800 <ericklimaflorencio@gmail.com>
2023-09-18 11:15:26 -0300

    delete, post e http

--- api/routes/prestador.js
+++ api/routes/prestador.js
@@ -1,10 +1,26 @@
 /* API REST dos prestadores */
 import express from "express"
 import { connectToDatabase } from '../utils/mongodb.js'
+import { check, ExpressValidator, validationResult } from "express-validator"
 
 const router = express.Router()
 const {db, ObjectId} = await connectToDatabase()
 const nomeCollection = 'prestadores'
+
+const validaPrestador = [
+    check('cnpj').not()
+    .isEmpty().trim().withMessage('É obrigatório informar o CNPJ')
+    .isNumeric().withMessage('O CNPJ só deve conter números')
+    .isLength({min: 14, max: 14}).withMessage('O CNPJ deve conter 14 n°s'),
+    check('razao_social').not()
+    .isEmpty().trim().withMessage('É obrigatório informar a razão social')
+    .isAlphanumeric('pt-BR', {ignore: '/. '}).withMessage('A razão social não deve conter caracteres especiais')
+    .isLength({min: 5}).withMessage('A razão social é muito curta. Minimo de 5 caracteres')
+    .isLength({max: 200}).withMessage('A razão social é muito longa. Maximo de 200 caracteres'),
+    check('cnae_fiscal')
+    .isNumeric().withMessage('O código do CENAE deve ser um número'),
+    check('nome_fantasia').optional({nullable: true})
+]
 
 /*
 * GET /api/prestadores
@@ -43,6 +59,35 @@ router.get('/id/:id', async(req, res) => {
         })
     }catch(err){
         res.status(500).json({"error": err.message})
+    }
+})
+
+/*
+* DELET /api/prestadores/:id
+* Apaga o prestadore de serviço pela id
+*/
+router.delete('/:id', async(req, res) => {
+    await db.collection(nomeCollection)
+    .deleteOne({"_id": {$eq: ObjectId(req.params.id)}})
+    .then(result => res.status(200).send(result))
+    .catch(err => res.status(400).json(err))
+})
+
+/*
+* POST /api/prestadores
+* Insere um novo prestador de serviço
+*/
+router.post('/', validaPrestador, async(req, res) =>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json(({
+            errors: errors.array()
+        }))
+    }else{
+        await db.collection(nomeCollection)
+        .insertOne(req.body)
+        .then(result => res.status(200).send(result))
+        .catch(err => res.status(400).json(err))
     }
 })
 
